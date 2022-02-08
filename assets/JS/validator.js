9c9d5468bfec146307910d6e5051e249a157ddea
thanleanhtuan <thanleanhtuan4@gmail.com>
2022-02-08 22:47:18 +0700

    updated account_film và validator

--- assets/JS/validator.js
+++ assets/JS/validator.js
@@ -3,10 +3,21 @@ function Validator(options) {
     // Lấy element của form cần validate
     var formElement = document.querySelector(options.form);
 
+    var selectorRules = {};
+
     // Thực hiện xác nhận dữ liệu nhập vào input
     function Validate(inputElement, rule) {
-        var errorMessage = rule.test(inputElement.value);
+        var errorMessage;
         var errorElement = inputElement.parentNode.querySelector(options.errorMessage);
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+        // Lặp qua các rule đã lấy và kiểm tra
+        for(var i = 0; i < rules.length; i++) {
+            errorMessage = rules[i](inputElement.value);
+            if(errorMessage) break;
+        }
+
         if(errorMessage) {
             errorElement.innerHTML = errorMessage;
             inputElement.parentNode.classList.add('invalid');
@@ -17,8 +28,27 @@ function Validator(options) {
     }
 
     if(formElement) {
+        formElement.onsubmit = function (e) {
+            // Tắt hành vi mặc định của submit
+            e.preventDefault();
+
+            // Thực hiện validate của form khi submit
+            options.rules.forEach(function(rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                Validate(inputElement, rule);
+            });
+        }
+
+
         options.rules.forEach(function(rule) {
             var inputElement = formElement.querySelector(rule.selector);
+
+            // Lưu các rule có cùng selector vào mảng
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
 
             if(inputElement) {
                 // Xử lý khi blur khỏi input
